0b521031848d508d9a59530cc6c50e5764ae16ad
Kraahkanite <saidmichael@users.noreply.github.com>
2006-10-01 19:03:51 +0000

    Mootools.

--- admin/admin-functions.php
+++ admin/admin-functions.php
@@ -146,15 +146,13 @@ function get_admin_header() {
 		<link rel="stylesheet" href="blackjack.css" type="text/css" />
 		<!-- tinyMCE -->
 		<script language="javascript" type="text/javascript" src="../jscripts/tiny_mce/tiny_mce.js"></script>
+		<script language="javascript" type="text/javascript" src="../jscripts/mootools.js"></script>
 		<script language="javascript" type="text/javascript">
 		tinyMCE.init({
 			theme : "advanced",
 			mode : "exact",
 			elements : "textarea",
 			extended_valid_elements : "a[href|target|name]",
-			//plugins : "table",
-			//theme_advanced_buttons1_add_before : "tablecontrols,separator",
-			//theme_advanced_styles : "Header 1=header1;Header 2=header2;Header 3=header3;Table Row=tableRow1", Theme specific setting CSS classes
 			debug : false,
 			theme_advanced_toolbar_location : "top",
 			theme_advanced_toolbar_align : "left",
