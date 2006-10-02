e8dd2ebbbea965d66889fa763a6f400525495a5b
Kraahkanite <saidmichael@users.noreply.github.com>
2006-10-02 01:33:02 +0000

    Ajax deleter.

--- admin/posts.php
+++ admin/posts.php
@@ -2,10 +2,10 @@
 $parent_file = "posts.php";
 require("admin-head.php");
 if(we_can('edit_posts')) {
-	get_admin_header();
 	switch($_GET['req']) {
 		case "edit" :
 			if(isset($_GET['id'])) {
+				get_admin_header();
 				$posts = get_posts('id='.intval($_GET['id']).'&limit=1');
 				foreach($posts as $post) { start_post(); ?>
 		<div id="wrapper">
@@ -13,17 +13,47 @@ if(we_can('edit_posts')) {
 <?php do_editorform($post); ?>
 		</div>
 <?php			}
-			}	
+				get_admin_footer();
+			}
 			break;
 	
 		case "delete" :
-		
+			if(isset($_GET['id'])) {
+				$bj_db->query("DELETE FROM `".$bj_db->posts."` WHERE `ID` = '".intval($_GET['id'])."' LIMIT 1");
+				@header("Location: ".load_option('siteurl')."admin/posts.php");
+				die();
+			}
+			break;
+			
+		case "ajaxdelete" :
+			if(isset($_GET['id'])) {
+				$bj_db->query("DELETE FROM `".$bj_db->posts."` WHERE `ID` = '".intval($_GET['id'])."' LIMIT 1");
+				_e('Post deleted.');
+			}
 			break;
 		
 		default :
+			#Attach this for ajax deleting.
+			function add_ajax_fun() { ?>
+		<script language="javascript" type="text/javascript">
+		confirmus = function(text,xml,thing){
+			document.getElementById("ajaxmessage").innerHTML="<strong class=\"error\">" + text +"</strong>";
+		};
+		deletePost = function(id){
+			var delCall = new Ajax('posts.php?req=ajaxdelete&id='+id,{onComplete:confirmus});
+			delCall.request();
+			var hideThis = $('post-'+id).effect('opacity', {duration: 750, transition: fx.linear});
+			hideThis.custom(1.0,0.2);
+		};
+		</script>
+<?php
+			}
+			add_action('admin_header','add_ajax_fun');
+			get_admin_header();
 ?>
 		<div id="wrapper">
 			<h1><?php _e('Manage Posts'); ?></h1>
+			<div id="ajaxmessage"></div>
 <?php
 			$drafts = $bj_db->get_rows("SELECT `ID`,`title` FROM `".$bj_db->posts."` WHERE `ptype` = 'draft' ORDER BY `ID` DESC","ASSOC");
 			if($drafts) { ?>
@@ -49,22 +79,22 @@ if(we_can('edit_posts')) {
 				</tr>
 <?php			$posts = get_posts('limit=16&type=public');
 				foreach($posts as $post) { start_post(); ?>
-				<tr<?php tablealt($i); ?>>
+				<tr<?php tablealt($i); ?> id="post-<?php echo $post['ID']; ?>">
 					<td class="aligncenter"><?php echo $post['ID']; ?></td>
 					<td><?php echo $post['title']; ?></td>
 					<td><?php post_date("M dS Y, h:i a"); ?></td>
 					<td><?php echo_tags(", ","","","admin=true"); ?></td>
 					<td class="capitalize aligncenter"><?php echo $post['ptype']; ?></td>
 					<td class="editbutton"><a href="posts.php?req=edit&amp;id=<?php echo $post['ID']; ?>" class="blockit"><?php _e('Edit'); ?></a></td>
-					<td class="editbutton"><a href="posts.php?req=delete&amp;id=<?php echo $post['ID']; ?>" class="blockit"><?php _e('Delete'); ?></a></td>
+					<td class="editbutton"><a href="posts.php?req=delete&amp;id=<?php echo $post['ID']; ?>" class="blockit" onclick="deletePost(<?php echo $post['ID']; ?>);return false;"><?php _e('Delete'); ?></a></td>
 				</tr>
 <?php			} ?>
 			</table>
 			<h3 class="gothere"><a href="post-write.php"><?php _e('Create a Post'); ?> &gt;&gt;</a></h3>
 		</div>
 <?php
+		get_admin_footer();
 	}
-	get_admin_footer();
 }
 else {
 	_e('You don\'t have permission to access this file.');
