dd6689583bc2d60bbd889005ecd1656cef0d3996
Kraahkanite <Kraahkanite@1db9d99e-a61c-0410-b778-492ba638feed>
2006-10-18 23:16:20 +0000

    Pretty URLs.

--- trunk/bj_config.php
+++ trunk/bj_config.php
@@ -5,7 +5,7 @@ define('DB_USER','root'); # Database user
 define('DB_PASS',''); # Database user's password
 define('DB_HOST','localhost'); # The host. Localhost should do just fine.
 
-#define('BJ_REWRITE',true); # This will make links turn into pretty URLs if uncommented.
+define('BJ_REWRITE',true); # This will make links turn into pretty URLs if uncommented.
 
 
 define('BJ_LANG',''); # Use this to localize Blackjack. Drop the language
