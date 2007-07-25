3c158c4f3d03f76fd69f8be6db74fd6d0ebab65c
Kraahkanite <saidmichael@users.noreply.github.com>
2007-07-25 17:49:23 +0000

    * Fix the allowed_tags function. * Fix up the default template a bit so functions stay updated. * If there are no entries in a tag or section, use a correct 404. * Keep outsiders out of /core.

--- content/skins/twentyone/section-single.php
+++ content/skins/twentyone/section-single.php
@@ -3,39 +3,19 @@
  Handler: Single Post
  */
 $hide_archive = 1;
-?>
-<?php skin_header(); ?>
-
+skin_header(); ?>
 			<div id="content">
 				<div id="primary" class="section-single">
-<?php
-				if($entries) { ?>
 					<div class="page-head">
-						<h2><?php
-						if(is_tag()) {
-							printf(_r('<span>Tag Archive: </span>%1$s'),wptexturize($tag['name']));
-						}
-						elseif(is_section()) {
-							echo wptexturize($section['title']);
-						} ?></h2>
+						<h2><?php echo wptexturize($section['title']); ?></h2>
 					</div>
 <?php
-					foreach($entries as $entry) { start_entry(); //$entry should stay $entry. Don't change. ?>
+					foreach($entries as $entry) { thru_loop(); //$entry should stay $entry. Don't change. ?>
 					<div class="entry-content">
-						<?php echo_content(); ?>
+						<?php entry_content(); ?>
 					</div>
 <?php
-					}
-				}
-				else { ?>
-					<div class="page-head">
-						<h2><?php _e('Not Found'); ?></h2>
-					</div>
-					<div class="entry-content">
-						<p><?php _e('Looks like you\'re out of luck: whatever you were looking for isn\'t here. Luckily, there are tons of tools around the website to help you find what you seek- a good deal of which, in fact, are right on this page.'); ?></p>
-					</div>
-<?php
-				} ?>
+					} ?>
 				</div>
 			</div>
 <?php skin_sidebar(); ?>
